83ee0cdde40cdcaab26550562650cf003cc3def8
Harshavardhan86 <harshavardhanpenta07@gmail.com>
2026-04-20 17:11:05 +0530

    added folder structure

---
+++ src/test/java/com/interview/aiinterviewcoach/AiInterviewCoachApplicationTests.java
@@ -0,0 +1,13 @@
+package com.interview.aiinterviewcoach;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiInterviewCoachApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
